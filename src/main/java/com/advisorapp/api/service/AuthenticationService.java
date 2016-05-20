82509dad61aaafd1fd66c9e39186becf10732d34
Damien RAYMOND <contact@damien-raymond.fr>
2016-05-21 00:02:22 +0200

    [ADV-16] Remove useless log message

--- src/main/java/com/advisorapp/api/service/AuthenticationService.java
+++ src/main/java/com/advisorapp/api/service/AuthenticationService.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
