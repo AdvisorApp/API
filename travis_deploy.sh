279fd59645affe8a7cc35202717d5b64fe1717d3
Clément Garbay <clementgarbay@gmail.com>
2016-05-23 16:49:46 +0200

    Adding deploy script

---
+++ travis_deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROJECT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version`
+
+# Copy .war on the server
+sshpass -p $DEPLOY_PASS scp -v -P $DEPLOY_SSH_PORT "/home/travis/build/AdvisorApp/API/target/api-$PROJECT_VERSION.war" $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+
+# Run application war
+sshpass -p $DEPLOY_PASS ssh -p $DEPLOY_SSH_PORT $DEPLOY_USER:$DEPLOY_PASS@$DEPLOY_HOST "cd $DEPLOY_PATH && nohup java -Dspring.profiles.active=production -Dspring.datasource.username=$DATABASE_USER -Dspring.datasource.password=$DATABASE_PASS -jar api-$PROJECT_VERSION.war &"
