a9cafdc4e9ba3ddf976b5415b9f4a708b6a23b90
SinigagliaSteeve <steeve.sinigaglia@laposte.net>
2016-05-24 17:17:49 +0200

    add password attributes for User entity

--- src/main/java/com/advisorapp/api/model/User.java
+++ src/main/java/com/advisorapp/api/model/User.java
@@ -31,6 +31,9 @@ public class User {
     @Column
     private String email;
 
+    @Column(name = "password",nullable = false, length = 60)
+    private String password;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<StudyPlan> studyPlans;
@@ -87,5 +90,17 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStudyPlans(Set<StudyPlan> studyPlans) {
+        this.studyPlans = studyPlans;
     }
 }
