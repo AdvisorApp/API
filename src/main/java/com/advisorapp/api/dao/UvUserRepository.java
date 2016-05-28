38d197cb34eb50bc343a9a7afcbed3cdb488b2aa
Alexis <robinalexis@outlook.fr>
2016-05-28 23:11:20 +0200

    [ADV-27] add services to find uvUser

--- src/main/java/com/advisorapp/api/dao/UvUserRepository.java
+++ src/main/java/com/advisorapp/api/dao/UvUserRepository.java
@@ -1,12 +1,28 @@
 package com.advisorapp.api.dao;
 
 import com.advisorapp.api.model.User;
+import com.advisorapp.api.model.Uv;
 import com.advisorapp.api.model.UvUser;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Steeve on 20/05/2016.
  */
 public interface UvUserRepository extends PagingAndSortingRepository<UvUser,Long> {
+
+    @Query("select u from UvUser u where u.user = :user")
+    Set<UvUser> findByUser(@Param("user") User user);
+
+    @Query("select u from UvUser u where u.uv = :uv")
+    Set<UvUser> findByUV(@Param("uv") Uv uv);
+
+    @Query("select u from UvUser u where u.uv = :uv or u.user = :user")
+    UvUser findByUvAndUser(@Param("uv") Uv uv,
+                           @Param("user") User user);
 
 }
--- src/main/java/com/advisorapp/api/service/UvUserService.java
+++ src/main/java/com/advisorapp/api/service/UvUserService.java
@@ -1,6 +1,8 @@
 package com.advisorapp.api.service;
 
 import com.advisorapp.api.dao.UvUserRepository;
+import com.advisorapp.api.model.User;
+import com.advisorapp.api.model.Uv;
 import com.advisorapp.api.model.UvUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
+/*
+ * Sample service to demonstrate what the API would use to get things done
+ */
 @Service
 public class UvUserService {
 
@@ -34,6 +41,18 @@ public class UvUserService {
 
     public UvUser getUvUser(long id) {
         return uvUserRepository.findOne(id);
+    }
+
+    public Set<UvUser> getUvUserByUser(User user) {
+        return uvUserRepository.findByUser(user);
+    }
+
+    public Set<UvUser> getUvUserByUV(Uv uv) {
+        return uvUserRepository.findByUV(uv);
+    }
+
+    public UvUser getUvUserByUVAndUser(Uv uv, User user) {
+        return uvUserRepository.findByUvAndUser(uv, user);
     }
 
     public void updateUvUser(UvUser uvUser) {
